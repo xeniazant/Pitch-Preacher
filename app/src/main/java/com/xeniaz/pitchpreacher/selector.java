ac3399cce991385421e7b11dc89240f816578a11
xeniazant <xeniazant@gmail.com>
2019-12-03 15:07:24 -0500

    two views with working pitch detection

---
+++ app/src/main/java/com/xeniaz/pitchpreacher/selector.java
@@ -0,0 +1,14 @@
+package com.xeniaz.pitchpreacher;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class selector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selector);
+    }
+}
